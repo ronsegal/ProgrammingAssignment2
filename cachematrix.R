06fc92bc3c1090db670bbcfce1bb07ae0a3d2a74
ronsegal <ron@mindhut.com>
2015-02-20 16:40:35 +1300

    Assignment update complete added setdata

--- cachematrix.R
+++ cachematrix.R
@@ -3,7 +3,7 @@
 
 ## These functions enable the results of an 'expensive' operation, matrix inversion
 ## to be effectively cached by assigning the result object from the first calculation to 
-## a symbol in a containing environment rather than to the calculating function's local environment
+## a symbol in a parent environment rather than to the calculating function's local environment
 ## Consequently on subsequent calls to the calculating function if this cached result exists
 ## it is used rather than re-calculating the inversion.
 ## The approach makes use of the superassignment operator
@@ -12,19 +12,24 @@
 ## Write a short comment describing this function
 
 ## This function creates a special matrix, returning a list of associated functions that:
-## a. Returns the original matrix argument
-## b. Caches the result of an inversion calculation in a variable in the parent environment
-## c. Returns the cached inversion result from the parent environment (or NULL if it has not yet been created)
+## a. Resets the value of the matrix without running this function, i.e. creating a new matrix  
+## b. Returns the original matrix argument
+## c. Caches the result of an inversion calculation in a variable in the parent environment
+## d. Returns the cached inversion result from the parent environment (or NULL if it has not yet been created)
 
 makeCacheMatrix <- function(x = matrix()) {
 
     inv <- NULL # initialise inversion result variable in makeCacheMatrix function environment
-
+    
+    setdata <- function (y) { # used to (re) set the matrix value without initialising makeCacheMatrix
+        x <<- y
+        inv <<- NULL
+    }
     getdata <- function() x # return the matrix object to be inverted
     setinvert <- function(invmat) inv <<- invmat # cache the inverted matrix result in parent environment
     getinvert <- function() inv # return NULL or the cached inverted matrix result from parent environment
 
-    list(getdata = getdata, setinvert = setinvert, getinvert = getinvert) # return list of functions with same names
+    list(setdata=setdata, getdata = getdata, setinvert = setinvert, getinvert = getinvert) # return list of functions with same names
 
 }
 
@@ -35,6 +40,8 @@ makeCacheMatrix <- function(x = matrix()) {
 ## If the inversion has already been calculated the result is returned from cache
 ## If the inversion hasn't yet been calculated the function calculates the inversion
 ## and stores the result in cache
+
+## NB - This function does not use setdata
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
